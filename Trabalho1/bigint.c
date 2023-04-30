7b786529c94862e7967f937e31388416a5ae91b8
lucasdclemos <lucasdclemos@gmail.com>
2023-04-30 17:09:22 -0300

    Testes finalizados

--- Trabalho1/bigint.c
+++ Trabalho1/bigint.c
@@ -52,16 +52,22 @@ void big_sub(BigInt res, BigInt a, BigInt b){
 
 /* res = a * b */
 void big_mul(BigInt res, BigInt a, BigInt b){
-    big_val(res, 0);
-    unsigned char resto = 0;
-    int temp = 0;
-    for (int i = 0; i < 2*(sizeof(long)); i++){
-        for (int j = 0; j < 2*(sizeof(long)); j++){
-            temp = (a[i] * b[j]) + resto + res[i + j];
-            res[j + i] = (unsigned char) temp;
-            resto = temp >> 8; 
+    int i, j, auxiliar, resto;
+    BigInt temp, parcial;
+    big_val(temp, 0); //para que se possa fazer algo como a=a*b
+    for(i = 0; i < 16; i++){
+        big_val(parcial, 0);		// variavel para armazenar os resultados sem o overflow(parciais)
+        resto = 0;
+        for(j = i; j < 16; j++)
+        {
+            auxiliar = ((a[i] * b[j-i]) + resto);  // multiplica a com b e guarda em aux
+            parcial[j] = auxiliar & 0xFF; //  parcial armazena apenas o que cabe em um byte
+            resto = auxiliar >> 8;                
         }
+        big_sum(temp, temp, parcial);
     }
+    big_copia(res, temp);
+
 }
 
 /* res = a */
@@ -96,7 +102,6 @@ void big_shr(BigInt res, BigInt a, int n){
     }
     BigInt resAuxiliar = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     int quociente = n/8;
-    int resto = n%8;
     unsigned char sobra = 0x00;
     for (int i = quociente; i > 0; i--){
         for (int j = 0; j <= 15; j++){
@@ -104,10 +109,10 @@ void big_shr(BigInt res, BigInt a, int n){
         }
         big_copia (a, resAuxiliar);
     }
-    if (resto > 0){
+    if (n%8 > 0){
         for (int i = 15; i >= 0; i--){
-            resAuxiliar[i] = (a[i] >> n)|sobra;
-            sobra = a[i] << (8 - resto);
+            resAuxiliar[i] = (a[i] >> (n % 8))|sobra;
+            sobra = a[i] << (8 - (n % 8));
         }
     }
     big_copia (res, resAuxiliar);
@@ -115,31 +120,47 @@ void big_shr(BigInt res, BigInt a, int n){
 
 /* res = a >> n (aritmético)*/
 void big_sar(BigInt res, BigInt a, int n){
+    if (n == 0){
+        big_copia(res, a);
+        return;
+    }
     BigInt resAuxiliar;
+    int quociente = n/8;
+    unsigned char sobra = 0x00;
     if ((a[15] & 0x80) == 0x80){
         for (int i = 0; i <= 15; i++) {
             resAuxiliar[i] = 0xff;
+        }
+        for (int i = quociente; i > 0; i--){
+            for (int j = 0; j <= 15; j++){
+                resAuxiliar [15-(j+1)] = a[15-j];
+            }
+            big_copia (a, resAuxiliar);
+        }
+        if ((n % 8) > 0){
+            for (int i = 15; i >= 0; i--){
+                resAuxiliar[i] = ((signed char)a[i] >> (n % 8))|sobra;
+                sobra = a[i] << (8 - (n % 8));
+            }
         }
     } else {
         for (int i = 0; i <= 15; i++) {
             resAuxiliar[i] = 0x00;
         }
-    }
-    int quociente = n/8;
-    int resto = n%8;
-    unsigned char sobra = 0x00;
-    for (int i = quociente; i > 0; i--){
-        for (int j = 0; j <= 15; j++){
-            resAuxiliar [15-(j+1)] = a[15-j];
+        for (int i = quociente; i > 0; i--){
+            for (int j = 0; j <= 15; j++){
+                resAuxiliar [15-(j+1)] = a[15-j];
+            }
+            big_copia (a, resAuxiliar);
         }
-        big_copia (a, resAuxiliar);
-    }
-    if (resto > 0){
-        for (int i = 15; i >= 0; i--){
-            resAuxiliar[i] = ((signed char)a[i] >> n)|sobra;
-            sobra = a[i] << (8 - resto);
+        if ((n % 8) > 0){
+            for (int i = 15; i >= 0; i--){
+                resAuxiliar[i] = (a[i] >> (n % 8))|sobra;
+                sobra = a[i] << (8 - (n % 8));
+            }
         }
     }
+
     big_copia (res, resAuxiliar);
 }
 
